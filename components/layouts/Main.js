65a2045f7813045c5023ec196e521a15f126ad7a
Nithish Karthik <75248557+sudo-NithishKarthik@users.noreply.github.com>
2021-02-12 18:59:34 +0530

    Update Main.js

--- components/layouts/Main.js
+++ components/layouts/Main.js
@@ -7,7 +7,7 @@ export const Main = ({children}) => {
     const user = useAuthenticate();
     if(user){
         return (
-            <div >
+            <div className="container-fluid vh-100 vw-100">
             <StatusBar />
             <div className={`row d-flex flex-column overflow-hidden`}>
                 <Navbar />
