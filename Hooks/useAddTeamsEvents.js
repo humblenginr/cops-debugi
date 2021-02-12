173293dade0e75514e963630ffebd2ca29b3abde
Nithish Karthik <75248557+sudo-NithishKarthik@users.noreply.github.com>
2021-02-12 13:50:56 +0530

    Update useAddTeamsEvents.js

--- Hooks/useAddTeamsEvents.js
+++ Hooks/useAddTeamsEvents.js
@@ -4,8 +4,10 @@ export const useAddTeamsEvents = (teamsEvents,setEvents,events) => {
     useEffect(() => {
         if(teamsEvents){
             if(events){
+
                 setEvents(prev => {
                     const data = prev;
+                    console.log(prev);
                     const lastElemId = prev[prev.length-1].Id
                     teamsEvents.forEach((event,index) => {
                         event.Id = index+1+lastElemId;
