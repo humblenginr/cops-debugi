796870b77bbfbc97dcc8bc6a3d678ca786489e17
Nithish Karthik <75248557+sudo-NithishKarthik@users.noreply.github.com>
2021-02-12 15:09:53 +0530

    Update useAddTeamsEvents.js

--- Hooks/useAddTeamsEvents.js
+++ Hooks/useAddTeamsEvents.js
@@ -7,13 +7,20 @@ export const useAddTeamsEvents = (teamsEvents,setEvents,events) => {
 
                 setEvents(prev => {
                     const data = prev;
-                    console.log(prev);
-                    const lastElemId = prev[prev.length-1].Id
+                        if(prev.length){
+                     const lastElemId = prev[prev.length-1].Id
                     teamsEvents.forEach((event,index) => {
                         event.Id = index+1+lastElemId;
                         data.push(event);
                     });
                     return data;
+                        }
+                                 teamsEvents.forEach((event,index) => {
+                        event.Id = index+1+lastElemId;
+                        data.push(event);
+                    });
+                    return data;
+                    
                 })
 
             }
